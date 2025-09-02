9a2b1a38cf68700f0f0aa1473b6e1fc15a2de83a
Ian Pollack <ianjpollack@gmail.com>
2025-09-01 17:00:27 -0700

    csv endpoint uploading to the db

--- app/database.py
+++ app/database.py
@@ -1,7 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
-# from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 import os
 
@@ -9,14 +8,10 @@ DB_USER = os.getenv("POSTGRES_USER", "postgres")
 DB_PASSWORD = os.getenv("POSTGRES_PASSWORD", "postgres")
 DB_NAME = os.getenv("POSTGRES_DB", "postgres")
 DB_HOST = os.getenv("POSTGRES_HOST", "db")
+DB_PORT = os.getenv("POSTGRES_PORT", "5432")
 
-DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:5432/{DB_NAME}"
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-
-# async_engine = create_async_engine(DATABASE_URL, echo=True)
-# AsyncSessionLocal = sessionmaker(
-#     bind=engine, class_=AsyncSession, expire_on_commit=False
-# )
--- app/main.py
+++ app/main.py
@@ -44,32 +44,3 @@ def upload_csv(file: UploadFile = File(...), db: Session = Depends(get_db)):
         )
     db.commit()
     return {"status": f"{len(df)} rows inserted successfully"}
-
-
-# @app.post("/upload-csv/")
-# def upload_csv(file: UploadFile = File(...)):
-#     df = pd.read_csv(file.file, encoding="latin-1")
-#     with SessionLocal() as session:
-#         for _, row in df.iterrows():
-#             session.execute(
-#                 text(
-#                     """
-#                     INSERT INTO provider_data (
-#                         Rndrng_Prvdr_CCN, Rndrng_Prvdr_Org_Name, Rndrng_Prvdr_City,
-#                         Rndrng_Prvdr_St, Rndrng_Prvdr_State_FIPS, Rndrng_Prvdr_Zip5,
-#                         Rndrng_Prvdr_State_Abrvtn, Rndrng_Prvdr_RUCA, Rndrng_Prvdr_RUCA_Desc,
-#                         DRG_Cd, DRG_Desc, Tot_Dschrgs, Avg_Submtd_Cvrd_Chrg, Avg_Tot_Pymt_Amt,
-#                         Avg_Mdcr_Pymt_Amt
-#                     ) VALUES (
-#                         :Rndrng_Prvdr_CCN, :Rndrng_Prvdr_Org_Name, :Rndrng_Prvdr_City,
-#                         :Rndrng_Prvdr_St, :Rndrng_Prvdr_State_FIPS, :Rndrng_Prvdr_Zip5,
-#                         :Rndrng_Prvdr_State_Abrvtn, :Rndrng_Prvdr_RUCA, :Rndrng_Prvdr_RUCA_Desc,
-#                         :DRG_Cd, :DRG_Desc, :Tot_Dschrgs, :Avg_Submtd_Cvrd_Chrg, :Avg_Tot_Pymt_Amt,
-#                         :Avg_Mdcr_Pymt_Amt
-#                     )
-#                 """
-#                 ),
-#                 row.to_dict(),
-#             )
-#         session.commit()
-#     return {"status": "CSV data inserted successfully"}
